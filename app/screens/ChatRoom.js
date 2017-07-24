963cb9e43482cf35130d07c65162adab7a34f639
pigflymoon <pplflymoon2015@gmail.com>
2017-07-24 15:21:37 +1200

    ChatRoom clean

--- app/screens/ChatRoom.js
+++ app/screens/ChatRoom.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import {
-    Platform,
-    StyleSheet,
     Text,
     TouchableOpacity,
 } from 'react-native';
 import {Router, Scene} from 'react-native-router-flux';
 import {Actions} from 'react-native-router-flux';
 
-import {bind} from '../utils/utils';
 import firebaseApp from '../config/FirebaseConfig';
 
 import Signin from '../components/Signin';
@@ -16,29 +13,27 @@ import Signup from '../components/Signup';
 import ChatGroup from '../components/ChatGroup';
 import VerifyEmail from '../components/VerifyEmail';
 import ResetPassword from '../components/ResetPassword';
+
 import chat from '../styles/chat';
 
 export default class ChatRoom extends Component {
     constructor(props) {
         super(props);
-        bind(this)('signout');
     }
 
-    signout() {
+    signout = () => {
         firebaseApp.auth().signOut().then(function () {
             Actions.signin();
         }).catch(function (error) {
             console.log('sign out error', error);
         })
-
-
     }
 
     render() {
         return (
             <Router barButtonIconStyle={chat.barButtonIconStyle}>
                 <Scene key='root'>
-                    <Scene key='chat'  component={ChatGroup} hideNavBar={false} hideBackImage={true}
+                    <Scene key='chat' component={ChatGroup} hideNavBar={false} hideBackImage={true}
                            renderRightButton={
                                () => <TouchableOpacity
                                    onPress={this.signout}>
@@ -48,15 +43,10 @@ export default class ChatRoom extends Component {
                     />
                     <Scene key='signin' title='Sign in' component={Signin} hideNavBar={true}/>
                     <Scene key='resetPassword' title='Reset Password' component={ResetPassword} hideNavBar={true}/>
-                    <Scene key='signup' title='Sign up' component={Signup} hideNavBar={true}>
-                    </Scene>
-                    <Scene key='verifyEmail' title='verifyEmail' component={VerifyEmail}
-
-
-                    />
+                    <Scene key='signup' title='Sign up' component={Signup} hideNavBar={true}/>
+                    <Scene key='verifyEmail' title='verifyEmail' component={VerifyEmail}/>
                 </Scene>
             </Router>
-        )
-            ;
+        );
     }
 }
